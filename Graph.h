baf09ded53bdc949a206bbd66c7a242a72f08920
TiagoTavaresSimoes <92669923+TiagoTavaresSimoes@users.noreply.github.com>
2023-04-10 14:54:36 +0100

    graph.h documentation updated

--- Graph.h
+++ Graph.h
@@ -14,17 +14,138 @@
 
 class Graph {
 public:
+
+
+    /**
+     * @brief Adds a station to the graph if it doesn't already exist.
+     * @param station The station object to be added.
+     * @return True if the station was added, False if the station already exists.
+     * @note Time-complexity: O(V) (where V is the number of vertices (stations)).
+     */
     bool addStation(const Station& station);
+
+
+
+
+
+
+    /**
+     * @brief Adds a railway between the source and destiny stations if they both exist.
+     * @param sourceStation The name of the source station.
+     * @param destinyStation The name of the destiny station.
+     * @param railway The railway object to be added.
+     * @return True if the railway was added, False if either source or destiny stations do not exist.
+     * @note Time-complexity: O(V) (where V is the number of vertices (stations)).
+     */
     bool addRailway(const std::string& sourceStation, const std::string& destinyStation, const Railway& railway) const;
+
+
+
+
+
+
+
+    /**
+     * @brief Adds a bidirectional railway between the source and destiny stations if they both exist.
+     * @param sourceStation The name of the source station.
+     * @param destinyStation The name of the destiny station.
+     * @param railway The railway object to be added.
+     * @return True if the bidirectional railway was added, False if either source or destiny stations do not exist.
+     * @note Time-complexity: O(V) (where V is the number of vertices (stations)).
+     */
     bool addBidirectionalRailway(const std::string& sourceStation, const std::string& destinyStation, const Railway& railway) const;
+
+
+
+
+
+
+
+    /**
+     * @brief Searches for a station in the graph by its name.
+     * @param name The name of the station to be found.
+     * @return Pointer to the Station if found, nullptr if not found.
+     * @note Time-complexity: O(V) (where V is the number of vertices (stations))
+     */
     Station* findStation(const std::string &name) const;
+
+
+
+
+
+
+
+    /**
+     * @brief Retrieves the list of stations in the graph.
+     * @return Vector of pointers to the Station objects in the graph.
+     * @note Time-complexity: O(1).
+     */
     std::vector<Station*> getStations() const;
+
+
+
+
+
+
+    /**
+    * @brief Removes a station from the graph along with its incoming and outgoing railways.
+    * @param name The name of the station to remove.
+    * @return true if the station was removed successfully, false otherwise.
+    * @note Time-complexity: O(V + E) (where V is the number of vertices and E is the number of edges connected to the station being removed).
+    */
     bool removeStation(const std::string& name);
+
+
+
+
+
+    /**
+     * @brief Calculates the number of unique districts in the graph.
+     * @return The number of unique districts in the graph.
+     * @note Time-complexity: O(V^2) (where V is the number of vertices).
+     */
     int getDistrictNumber() const;
+
+
+
+
+
+
+
+    /**
+     * @brief Calculates the number of unique municipalities in the graph.
+     * @return The number of unique municipalities in the graph.
+     * @note Time-complexity: O(V^2) (where V is the number of vertices).
+     */
     int getMunicipalityNumber() const;
 
+
+
+
+
+    /**
+     * @brief Marks a station as visited and enqueues it for further processing if it has not been visited yet and the residual capacity is positive.
+     * @param queue Reference to the queue used for station processing.
+     * @param railway Pointer to the railway connecting the previously visited station with the current station.
+     * @param station Pointer to the station being visited.
+     * @param residualCapacity The residual capacity of the path leading to the current station
+     * @note Time-complexity: O(1) (constant time operations).
+     */
     void visitStation(std::queue<Station*> &queue, Railway* railway, Station* station, double residualCapacity);
+
+
+
+
+    /**
+     * @brief Finds a path with available capacity between the source and destiny stations using a modified Breadth-First Search (BFS) algorithm.
+     * @param source Pointer to the source station.
+     * @param destiny Pointer to the destiny station.
+     * @return true if a path with available capacity is found, false otherwise.
+     * @note Time-complexity: O(V + E) (where V is the number of vertices and E is the number of edges)
+     */
     bool findPath(Station* source, Station* destiny);
+
+
 
     /**
     * @brief Calculates the minimum residual capacity along the path from source to destiny station.
@@ -34,6 +155,10 @@ public:
     * @note Time complexity: O(|V|) for a graph with |V| vertices.
     */
     double minResidualCapacity(Station* source, Station* destiny);
+
+
+
+
     /**
     * @brief Augments the flow along the path from source to destiny station by the given residual capacity.
     * @param source Pointer to the source station.
@@ -42,6 +167,10 @@ public:
     * @note Time complexity: O(|V|) for a graph with |V| vertices.
     */
     void augmentFlow(Station* source, Station* destiny, double residualCapacity);
+
+
+
+
 
     /**
     * @brief Computes the maximum flow in a flow network using the Edmonds-Karp algorithm.
@@ -52,6 +181,8 @@ public:
     */
     double edmondsKarp(Station* sourceStation, Station* destinyStation);
 
+
+
     //Functions for the requests
     /**
     * @brief Calculates the maximum number of trains that can travel between two stations using the Edmonds-Karp algorithm.
@@ -61,9 +192,43 @@ public:
     * @note Time complexity: O(|V|^3) for a graph with |V| vertices, as it calls the edmondsKarp function.
     */
     double getTrainsBetweenStations(const std::string& source, const std::string& destiny);
+
+
+
+
+    /**
+     * @brief Calculates pairs of stations with the most trains between them and displays a progress bar while processing.
+     * @return A vector of station pairs with the highest number of trains between them.
+     * @note Time-complexity: O(V^2) (where V is the number of vertices (stations)).
+     */
     std::vector<std::pair<std::pair<std::string, std::string>, double>> pairsWithMostTrains();
+
+
+
+
+
+    /**
+     * @brief Calculates the total train flow for each district-municipality pair and returns a sorted vector of pairs (district, municipality) and their corresponding train flows in descending order.
+     * @return A sorted vector of pairs (district, municipality) and their corresponding train flows.
+     * @note Time-complexity: O(V^2) (where V is the number of vertices (stations))
+     */
     std::vector<std::pair<std::pair<std::string, std::string>, double>> largerBudgetsMunicipalities();
+
+
+
+
+
+    /**
+     * @brief Calculates the total train flow for each district and returns a sorted vector of district names and their corresponding train flows in descending order.
+     * @return A sorted vector of district names and their corresponding train flows.
+     * @note Time-complexity: O(V^2) (where V is the number of vertices (stations))
+     */
     std::vector<std::pair<std::string, double>> largerBudgetsDistricts();
+
+
+
+
+
     /**
     * @brief Calculates the total number of trains arriving at a given station using the Edmonds-Karp algorithm.
     * @param stationName The name of the station for which the arriving trains should be calculated.
@@ -71,6 +236,12 @@ public:
     * @note Time complexity: O(|V|^3) for a graph with |V| vertices, as it calls the edmondsKarp function.
     */
     double arrivingTrains(const std::string& stationName);
+
+
+
+
+
+
     /**
     * @brief Calculates the optimal cost of trains traveling between two stations using a cost variant of the Edmonds-Karp algorithm.
     * @param source The name of the source station.
@@ -79,6 +250,12 @@ public:
     * @note Time complexity depends on the implementation of the costEdmondsKarp function.
     */
     double optimalCostTrains(const std::string& source, const std::string& destiny);
+
+
+
+
+
+
     /**
     * @brief Computes the minimum cost for maximum flow in a flow network using a cost variant of the Edmonds-Karp algorithm.
     * @param sourceStation Pointer to the source station.
@@ -87,6 +264,12 @@ public:
     * @note Time complexity depends on the implementation of findPath, minResidualCapacityCost, and augmentFlow functions.
     */
     bool dijkstra(std::string& source, std::string& dest);
+
+
+
+
+
+
     /**
     * @brief Calculates the minimum residual capacity and cost along the path from source to destiny station.
     * @param source Pointer to the source station.
@@ -95,6 +278,11 @@ public:
     * @note Time complexity: O(|V|) for a graph with |V| vertices.
     */
     void printPath(Station* orig, Station* dest);
+
+
+
+
+
     /**
     *
     * @param source A reference to a string representing the name of the source station.
@@ -104,6 +292,16 @@ public:
     * @note Time complexity: O(|V|^2 * |E|) for a graph with |V| vertices and |E| edges, assuming the Edmonds-Karp algorithm is used to compute the maximum flow.
     */
     double getTrainsBetweenStationsReduced(const std::string &source, const std::string &destiny, const std::string &line, const std::vector<Railway*>& segmentsImpacted);
+
+
+
+
+    /**
+    * @brief Calculates the impact of a failure in a set of railway segments by comparing the normal and new global flow for each station.
+    * @param segmentsImpacted Vector of pointers to the railway segments impacted by a failure
+    * @return A sorted vector of pairs where the first element is a pair of station name and normal flow, and the second element is the changed flow due to the failure
+    * @note Time-complexity: O(V^2 * E) (where V is the number of vertices and E is the number of edges)
+    */
     std::vector<std::pair<std::pair<std::string, double>,double>> stationSegmentFailureImpact(const std::vector<Railway*>& segmentsImpacted);
 
 private:
